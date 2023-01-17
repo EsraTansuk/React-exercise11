b8fea508c63286aba71551634fe161e824261d3e
esra tansuk <esra.balkis@outlook.com>
2023-01-17 17:28:52 +0300

    react hook useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  // const [red, green, blue] = [9, 132, 227]
+
+
+  function increase() {
+    setCount(count + 1)
+  }
+
+  function decrease() {
+    setCount(count - 1)
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+  
+    </div>
+
+  );
 }
 
 export default App;
